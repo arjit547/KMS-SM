66a822d7977f711e0f4cc0e8687bd763dda5b5bf
arjit547 <97424724+arjit547@users.noreply.github.com>
2024-02-06 11:51:20 +0530

    new updated code 6-12

--- kmseventcode/kms8.py
+++ kmseventcode/kms8.py
@@ -8,11 +8,14 @@ regions = [region['RegionName'] for region in ec2_client.describe_regions()['Reg
 
 # Open a CSV file for writing
 with open('kms_encrypt_decrypt_events.csv', 'w', newline='') as csvfile:
-    fieldnames = ['Region', 'KeyID', 'Aliases', 'EventName', 'LastAccessed', 'Username', 'CreationDate']
+    fieldnames = ['Region', 'KeyID', 'Aliases', 'EventName', 'LastAccessed', 'Username', 'CreationDate', 'LastAccessedUsername']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     # Write header row to CSV file
     writer.writeheader()
+
+    # Dictionary to store the most recent event for each unique combination of 'KeyID'
+    recent_events = {}
 
     # Iterate over each region
     for region in regions:
@@ -31,37 +34,50 @@ with open('kms_encrypt_decrypt_events.csv', 'w', newline='') as csvfile:
             aliases_response = kms_client.list_aliases(KeyId=key_id)
             aliases = [alias['AliasName'] for alias in aliases_response.get('Aliases', [])]
 
-            # Fetch KMS key metadata to get the creation date
+            # Fetch KMS key metadata to get the creation date and creator's username
             key_metadata = kms_client.describe_key(KeyId=key_id)['KeyMetadata']
             creation_date = key_metadata['CreationDate']
+            creator_username = key_metadata.get('CreatedBy', {}).get('Username')
 
             # Fetch CloudTrail events for the specified KMS key
             cloudtrail_client = boto3.client('cloudtrail', region_name=region)
             response = cloudtrail_client.lookup_events(
                 LookupAttributes=[
                     {'AttributeKey': 'ResourceName', 'AttributeValue': key_id},
-                    {'AttributeKey': 'EventName', 'AttributeValue': 'Encrypt,Decrypt,ReEncrypt'},
                 ],
             )
 
             events = response.get('Events', [])
 
-            # Iterate over each event for the KMS key
+            # Find the most recent event for the KMS key
+            most_recent_event = None
             for event in events:
                 event_name = event.get('EventName')
+                event_time = event.get('EventTime')
+                username = event.get('Username')
 
                 # Check if the event is relevant to KMS operations
                 if event_name in ['Encrypt', 'Decrypt', 'ReEncrypt']:
-                    event_time = event.get('EventTime')
-                    username = event.get('Username')
+                    if most_recent_event is None or event_time > most_recent_event['LastAccessed']:
+                        most_recent_event = {
+                            'EventName': event_name,
+                            'LastAccessed': event_time,
+                            'Username': username,
+                        }
 
-                    # Write details to CSV file
-                    writer.writerow({
-                        'Region': region,
-                        'KeyID': key_id,
-                        'Aliases': ', '.join(aliases),
-                        'EventName': event_name,
-                        'LastAccessed': event_time,  
-                        'Username': username,
-                        'CreationDate': creation_date,
-                    })
+            # Update the recent event for this KMS key
+            if most_recent_event:
+                recent_events[key_id] = {
+                    'Region': region,
+                    'KeyID': key_id,
+                    'Aliases': ', '.join(aliases),
+                    'EventName': most_recent_event['EventName'],
+                    'LastAccessed': most_recent_event['LastAccessed'],
+                    'Username': creator_username,  
+                    'CreationDate': creation_date,
+                    'LastAccessedUsername': most_recent_event['Username'],  
+                }
+
+    # Write the most recent events to the CSV file
+    for event_data in recent_events.values():
+        writer.writerow(event_data)
